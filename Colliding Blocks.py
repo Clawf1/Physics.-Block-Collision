79a9ad271bf0c95e7733a28b6dd69fe15e3d58b2
Clawf1 <96695549+Clawf1@users.noreply.github.com>
2023-10-23 09:56:37 +0300

    Add files via upload

---
+++ Colliding Blocks.py
@@ -0,0 +1,78 @@
+from vpython import *
+
+mass_1 = 1  # kg
+mass_2 = 10000  # kg
+velocity_1 = 0  # m/s
+velocity_2 = -50  # m/s
+side = 2
+dt = 0.01
+
+
+def velocity_after_block_collision(m_1, m_2, v_10, v_20):
+    v_1 = (2 * m_2 * v_20 + v_10 * (m_1 - m_2)) / (m_1 + m_2)
+    v_2 = (2 * m_1 * v_10 + v_20 * (m_2 - m_1)) / (m_1 + m_2)
+    return v_1, v_2
+
+
+def velocity_after_wall_collision(v_10):
+    return -v_10
+
+
+# Scene
+scene = canvas(
+    title='Collisions', x=0, y=0, width=1000, height=300, center=vector(0, 0, 0), background=color.black, fov=0.05)
+floor = box(pos=vector(0, -10.5, 0), length=50, height=1, width=5, color=color.white)
+wall = box(pos=vector(-25.5, -6, 0), length=1, height=10, width=5, color=color.white)
+
+# Blocks
+block_1 = box(pos=vector(0, -8, 0), length=2 * side, height=2 * side, width=2 * side, color=color.red)
+block_1.velocity = vector(velocity_1, 0, 0)
+block_2 = box(pos=vector(23, -8, 0), length=2 * side, height=2 * side, width=2 * side, color=color.green)
+block_2.velocity = vector(velocity_2, 0, 0)
+
+# Labels
+collision_label = label(pos=vector(0, 8, 0), color=color.white, linecolor=color.black)
+velocity_1_label = label(pos=vector(0, 5, 0), color=color.red, linecolor=color.black)
+velocity_2_label = label(pos=vector(0, 2, 0), color=color.green, linecolor=color.black)
+
+# Simulation loop
+collision_count = 0
+time = 0
+count = 0
+
+if __name__ == '__main__':
+    while True:
+        rate(100)
+
+        if block_2.pos.x - side < block_1.pos.x + side:
+            velocity_1, velocity_2 = velocity_after_block_collision(
+                mass_1, mass_2, velocity_1, velocity_2
+            )
+            block_1.velocity.x = velocity_1
+            block_2.velocity.x = velocity_2
+            collision_count += 1
+
+        if block_1.pos.x - side <= wall.pos.x + 0.5:
+            velocity_1 = velocity_after_wall_collision(velocity_1)
+            block_1.velocity.x = velocity_1
+            collision_count += 1
+
+        block_1.pos += block_1.velocity * dt
+        block_2.pos += block_2.velocity * dt
+
+        if block_2.pos.x > 25:
+            block_2.visible = False
+
+        if block_1.pos.x > 25:
+            block_1.visible = False
+
+        if velocity_2 > velocity_1 >= 0:
+            count += 1
+            if count == 100:
+                break
+
+        time += dt
+
+        collision_label.text = f'Collisions: {collision_count}'
+        velocity_1_label.text = f'Velocity 1: {block_1.velocity.x:.2f} m/s'
+        velocity_2_label.text = f'Velocity 2: {block_2.velocity.x:.2f} m/s'
